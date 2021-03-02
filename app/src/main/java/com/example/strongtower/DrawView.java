4b234788da26311237c83bb92bfde79743e274cb
Vladislav <vlad_moryakov_prog@mail.ru>
2021-03-02 07:33:49 +0300

    Это стартовая версия приложения, в ней реализована общая концепция игры, пока только топорным образом. Что имеем: 1) Движущиеся квадраты 2) Смена дня и ночи 3) Стоящий дом
    Что реализовать ***нужно***
    1) Адаптивность
    2) Анимации бегающих человечков
    3) Нормальный дом
    4) Убийство человечков

---
+++ app/src/main/java/com/example/strongtower/DrawView.java
@@ -0,0 +1,50 @@
+package com.example.strongtower;
+
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class DrawView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private DrawThread drawThread;
+
+
+    public DrawView(Context context) {
+        super(context);
+        getHolder().addCallback(this);
+    }
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        drawThread = new DrawThread(getContext(), getHolder());
+        drawThread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        drawThread.requestStop();
+        boolean retry = true;
+        while (retry) {
+            try {
+                drawThread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+                //
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        drawThread.setTowardPoint((int) event.getX(), (int) event.getY());
+
+        return false;
+    }
+}
