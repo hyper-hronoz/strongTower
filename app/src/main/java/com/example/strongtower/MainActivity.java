cb1c3b229a6e2b5217c1a9a5ce25b4c24b34d775
Vladislav <vlad_moryakov_prog@mail.ru>
2021-03-04 22:06:51 +0300

    Добавил я полный экран

--- app/src/main/java/com/example/strongtower/MainActivity.java
+++ app/src/main/java/com/example/strongtower/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.strongtower;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.strongtower.drawBattlefield.*;
 
+import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new DrawView(this));
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
     }
 }
