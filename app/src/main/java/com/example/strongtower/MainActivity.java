318b1a61f1aacd3640277a4347a4f411e1c5f099
Vladislav <vlad_moryakov_prog@mail.ru>
2021-03-05 21:22:08 +0300

    Теперь приложение полностью полноэкранно

--- app/src/main/java/com/example/strongtower/MainActivity.java
+++ app/src/main/java/com/example/strongtower/MainActivity.java
@@ -6,6 +6,7 @@ import com.example.strongtower.drawBattlefield.*;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class MainActivity extends Activity {
@@ -15,7 +16,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(new DrawView(this));
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        // делаем полноэкранное
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 
     @Override
